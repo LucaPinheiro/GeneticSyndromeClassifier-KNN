851f3a98045cb03075c615fb23c48dc613c5d0a2
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-07 00:14:52 -0300

    add prediction controller with FastAPI endpoint for model predictions

---
+++ src/api/controller/prediction_controller.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+predict_endpoint = APIRouter()
+
+model = joblib.load("src/models/best_knn_cosine.pkl")
+
+class EmbeddingInput(BaseModel):
+    embedding: list[float]
+
+@predict_endpoint.post("/predict/")
+def predict(data: EmbeddingInput):
+    try:
+        if len(data.embedding) != 320:
+            return {"error": "O vetor de entrada deve ter exatamente 320 dimensões."}
+
+        input_array = np.array(data.embedding).reshape(1, -1)
+        predicted_class = model.predict(input_array)[0]
+
+        return {
+            "predicted_class": int(predicted_class),
+            "message": "Previsão realizada com sucesso!"
+        }
+    except Exception as e:
+        return {"error": str(e)}
