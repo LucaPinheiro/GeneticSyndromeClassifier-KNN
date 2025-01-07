6bc04f1de10438ad3adedb6752fce4db1a1e29f5
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-06 23:41:39 -0300

    remove commented-out test function from main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import numpy as np
 import pandas as pd
 from src.utils.predict import predict_single_embedding
-from tests.test_generalization import test_generalization_with_saved_model
 
 df = pd.read_csv('./data/processed/normalized_data.csv')
 X = df.iloc[:, 3:].values
 y = df['syndrome_id'].values
 
-# Teste de generalização
-test_generalization_with_saved_model(X, y, model_path='./src/models/best_knn_cosine.pkl')
 
 # Simulação de um novo vetor de 320 dimensões
 new_embedding = np.random.rand(320)  # Teste com um vetor random
