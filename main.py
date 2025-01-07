3af45114457e63019152a8ec6dc8d1f4ab42b0e2
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-07 00:14:39 -0300

    refactor main.py to implement FastAPI and set up prediction endpoint

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import numpy as np
-import pandas as pd
-from src.utils.predict import predict_single_embedding
+from fastapi import FastAPI
+from src.api.controller.prediction_controller import predict_endpoint
 
-df = pd.read_csv('./data/processed/normalized_data.csv')
-X = df.iloc[:, 3:].values
-y = df['syndrome_id'].values
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API de Previsão de Síndrome com FastAPI está rodando!"}
+
+app.include_router(predict_endpoint)
 
 
-# Simulação de um novo vetor de 320 dimensões
-new_embedding = np.random.rand(320)  # Teste com um vetor random
-
-# previsão usando o modelo
-predicted_class = predict_single_embedding(new_embedding, model_path='./src/models/best_knn_cosine.pkl')
-print(f"A síndrome predita para este novo dado é: {predicted_class}")
