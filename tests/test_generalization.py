14977d7ca480c703870ebc1d4d54009afe7cbff1
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-06 22:07:09 -0300

    remove outdated comment from generalization test function

--- tests/test_generalization.py
+++ tests/test_generalization.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 import joblib
 
 def test_generalization_with_saved_model(X, y, model_path='models/best_knn_final.pkl'):
-    """Teste de Generalização usando o modelo salvo (.pkl)"""
     X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=42)
     X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
